d22aa98f8e01e1193a3eed541bfa449570aa8991
dheeraj1524 <dheerajkanduri79@gmail.com>
2025-11-12 08:43:48 +0530

    Create VotingEligibility.java

---
+++ VotingEligibility.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class VotingEligibility {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+        System.out.print("Are you a citizen? (true/false): ");
+        boolean isCitizen = scanner.nextBoolean();
+
+        if (age >= 18 && isCitizen) {
+            System.out.println("You are eligible to vote.");
+        } else {
+            System.out.println("You are not eligible to vote.");
+        }
+    }
+}
